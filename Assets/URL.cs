25fbaf09dfc8b5792ee354315ce3a6b180b2857a
TempTasks <shane.pepperell@me.com>
2020-06-07 02:25:52 +0100

    Link to wiki

---
+++ Assets/URL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URL : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void OpenHelp()
+    {
+        Application.OpenURL("https://github.com/OfficiallySp777/TempTasks/wiki");
+    }
+}
